a5fb7339a0ec118c057decfba863b1fe4b163b65
Ask <34611138+FaNzu@users.noreply.github.com>
2022-12-01 10:33:02 +0100

    Ex33 Alle opgaver med Academy og delegate

---
+++ AllProjects/Acadamy.cs
@@ -0,0 +1,36 @@
+﻿using AllProjects.Opg2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllProjects
+{
+    public class Academy : Organization
+    {
+        public NotifyHandler MessageChanged;
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                message = value;
+                OnMessageChanged();
+            }
+        }
+
+
+        public Academy(string name, string address) : base(name)
+        {
+            Adress = address;
+        }
+
+        public void OnMessageChanged()
+        {
+            MessageChanged();
+        }
+    }
+}
